4cd5e876e3f2b2fbe0d4b3ac2142326ddad6a1a2
Ben Greenberg <bengreenberg@gmail.com>
2023-04-28 11:15:37 +0300

    attempting to make work

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './Chatbot.css';
+import { RetrievalQAChain, loadQARefineChain } from 'langchain/chains';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'; // Import the OpenAIEmbeddings class
+import { MemoryVectorStore } from 'langchain/vectorstores/memory';
 
 const Chatbot = () => {
   const [input, setInput] = useState('');
@@ -8,24 +11,44 @@ const Chatbot = () => {
 
   useEffect(() => {
     const loadChain = async () => {
-      const savedChain = await fetch('/chatbot-chain.json').then((response) => response.json());
-      setChain(savedChain);
+      // Load vector store data and model data from your files or server.
+      const vectorStoreData = await fetch('/chatbot-vectorstore.json').then((res) => res.json());
+      const modelData = await fetch('/chatbot-model.json').then((res) => res.json());
+      const openAIEmbeddings = new OpenAIEmbeddings({ ...modelData, openAIApiKey: process.env.OPENAI_API_KEY });
+
+      // Create an instance of MemoryVectorStore.
+      const vectorStore = new MemoryVectorStore({ data: vectorStoreData.data, embeddings: openAIEmbeddings });
+
+      // Create an instance of RetrievalQAChain.
+      const combineDocumentsChain = loadQARefineChain(openAIEmbeddings);
+      const chainInstance = new RetrievalQAChain({
+        combineDocumentsChain,
+        retriever: vectorStore.asRetriever(),
+      });
+
+      setChain(chainInstance);
     };
     loadChain();
   }, []);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
-    if (!input || !chain) return;
-
+  
+    if (!input) return;
+  
     setMessages([...messages, { type: 'user', text: input }]);
     setInput('');
-
-    const response = await chain.call({ query: input });
-    setMessages([...messages, { type: 'user', text: input }, { type: 'bot', text: response.output_text }]);
+  
+    // Check if the chain object is defined and has a call method
+    if (chain && typeof chain.call === 'function') {
+      const response = await chain.call({ query: input });
+      setMessages([...messages, { type: 'user', text: input }, { type: 'bot', text: response.text }]);
+    } else {
+      console.log('Chain object is not defined or does not have a call method:', chain);
+      console.log(chain.constructor.name);
+    }
   };
-
+  
   return (
     <div className="chatbot">
       <div className="chatbot-messages">
